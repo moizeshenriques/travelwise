432438bcc23498899902855e512b276690a01a4f
cmarte03 <144187822+cmarte03@users.noreply.github.com>
2023-12-02 17:48:00 -0500

    Update smartSearch.php

--- smartSearch.php
+++ smartSearch.php
@@ -3,9 +3,199 @@
 <meta charset="utf-8">
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
-<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:400,500,700&display=swap">
-<link rel="stylesheet" href="styles.css">
- 
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href='https://fonts.googleapis.com/css?family=Poppins'>
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Poppins&display=swap');
+
+body {
+    margin: 0;
+    padding: 0;
+    font-family: 'Poppins', sans-serif;
+}
+
+header {
+    color: #fff;
+    padding: 15px 0;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+}
+
+.logo {
+    display: flex;
+    align-items: center;
+}
+
+.logo img {
+    width: 25%;
+    height: 25%;
+    margin-right: 10px;
+    margin-left: 50px;
+}
+
+nav {
+    display: flex;
+    align-items: center;
+}
+
+nav a {
+    color: #112211;
+    text-decoration: none;
+    margin: 0 15px;
+    font-weight: 500;
+    white-space: nowrap;
+    font-family: 'Poppins', sans-serif;
+}
+
+footer {
+    background-color: #FEEDE1;
+    padding: 30px 0;
+    color: #112211;
+    display: flex;
+    justify-content: space-between;
+    align-items: flex-start;
+    flex-wrap: wrap;
+}
+
+.subscribe-container {
+    max-width: 40%;
+    margin-left: 50px;
+}
+
+.subscribe-text {
+    margin-bottom: 20px;
+    font-size: small;
+}
+
+.form-container {
+    display: flex;
+    align-items: center;
+}
+
+.email-input {
+    width: 80%;
+    padding: 8px;
+    border: 1px solid #5842A3;
+    border-radius: 4px;
+    margin-right: 5px; 
+    font-family: 'Poppins', sans-serif;
+}
+
+.subscribe-button {
+    background-color: #5842A3;
+    color: #fff;
+    padding: 8px 10px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    font-family: 'Poppins', sans-serif;
+}
+
+.page-links {
+    max-width: 40%;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-end; 
+    margin-right: 50px; 
+    margin-bottom: 15px;
+    margin-top: 50px;
+    margin-left: 100px;
+}
+
+.page-links a {
+    text-decoration: none;
+    margin-bottom: 5px;
+    color: #112211;
+    font-family: 'Poppins', sans-serif;
+}
+
+.logo-small {
+    width: 15%; /* Adjust size as needed */
+    height: 15%;
+    margin-left: 300px;
+    margin-top: 50px;
+    margin-right: 50px;
+}
+
+.copyright {
+    font-size: smaller;
+    margin-left: 50px;
+    margin-top: 5px;
+    max-width: 80%;
+}
+
+/* smart search styling */
+#smart-search-title h1, #your-dest-title h1 {
+    color: #5842A3;
+    font-style: normal;
+    font-weight: 700;
+    margin-left: 50px;
+    font-size: 32px;
+}
+
+#smart-search-title p {
+    font-size: 18px;
+    margin-left: 50px;
+    margin-right: 50px;
+}
+
+.smart-form-container {
+    margin-left: 50px;
+}
+
+#travelBudget, #numDays, #numTravelers {
+    width: 300px;
+    height: 50px;
+    border: 2px solid rgba(0, 0, 0, 0.40);
+    flex-shrink: 0;
+    background: #FFF;
+    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+    color: #1E1D4C;
+    font-family: Poppins;
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    margin-left: 10px;
+    border-radius: 20px;
+    padding: 8px;
+}
+
+.smart-form {
+    display: flex;
+}
+
+#submitButton {
+    width: 300px;
+    height: 50px;
+    border: 2px solid rgba(0, 0, 0, 0.40);
+    flex-shrink: 0;
+    background: #F76A09;
+    box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+    color: #FFF;
+    font-family: Poppins;
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    margin-left: 10px;
+    border-radius: 20px;
+    text-align: center;
+}
+
+form {
+    display: flex;
+    flex-direction: column;
+}
+
+.luggage-pic {
+    margin-left: 20%;
+    margin-right: 50px;
+}
+
+</style>
 <header>
     <div class="logo">
         <img src="logotravelwise.png">
@@ -17,9 +207,12 @@
         <a href="#">Budgeting Hacks</a>
     </nav>
 </header>
-
-<h1>Smart Search</h1>
-<p>Unlock budget-friendly destinations with Travel Wise's Smart Search—your gateway to affordable exploration, curated for unforgettable experiences within your means</p>
+<section id="smart-search-section">
+    <div id="smart-search-title">
+        <h1>Smart Search</h1>
+        <p>Unlock budget-friendly destinations with Travel Wise's Smart Search—your gateway to affordable exploration, curated for unforgettable experiences within your means</p>
+    </div>
+</section>
 </head>
 <body>
    <?php
@@ -27,17 +220,20 @@
     ?>
     <br>
     <!-- create order form -->
-    <div class = "smart-search-form">
-    <h1>Order Form</h1>
+    <div class="smart-form-container">
+        <div class="smart-form">
+        <div class="form">
         <input type="number" id="travelBudget" name = "travelBudget" placeholder="Total Travel Budget" min="1" required><br><br>
         <input type="number" id="numDays" name = "numDays" placeholder="Number of days" min="1" required><br><br>
         <input type="number" id="numTravelers" name="numTravelers" placeholder="Number of travelers" min="1" required><br><br>
         <input type="submit" id="submitButton" value="Search">
     </form>
+        </div>
+        <img src="luggage.png" width="500" height="400" class="luggage-pic">
+        </div>
     </div>
     <br>
     <br>
-    
     <footer>
         <div class="subscribe-container">
             <div class="subscribe-text">
