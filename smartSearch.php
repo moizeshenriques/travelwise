0d2600f3df39f0c42c20cc84412e74000b75707d
marina-garrido <144355207+marina-garrido@users.noreply.github.com>
2023-12-10 15:46:18 -0500

    Update smartSearch.php

--- smartSearch.php
+++ smartSearch.php
@@ -4,6 +4,7 @@
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <link rel="preconnect" href="https://fonts.googleapis.com">
+ <meta name="keywords" content="travel, international travel, travel budget calculator, currency rates, budgeting, cheap destinations, travel packages">
 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Volkhov&display=swap">
  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Poppins:400,500,700&display=swap">
